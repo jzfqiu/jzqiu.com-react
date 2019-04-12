59c0f068b50e2253dc5c9b19c340d3d60867e2fd
Jeff Qiu <zqiu@uchicago.edu>
2019-04-12 13:37:56 -0500

    added basic react project layout

---
+++ src/heya.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+
+class Heya extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Heya.</h1>
+                <p>This is Zhenfeng Qiu. You can also call me Jeff.</p>
+                <p>I am a college student studying Computer Science and Economics.</p>
+                <p>I like to daydream, build stuff, and play with toys.</p>
+                <p>Also, I like dolphins.</p>
+            </div>
+        );
+    }
+}
+
+export default Heya;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Heya from './heya.js';
+
+ReactDOM.render(<Heya />, document.getElementById('HeyaContainer'));
+
+
