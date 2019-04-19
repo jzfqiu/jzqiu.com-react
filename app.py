5933041482019422e32ff857be17d745bf7cdfc2
Jeff Qiu <zqiu@uchicago.edu>
2019-04-18 23:21:10 -0500

    rm stuff

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__, static_folder="build/static", template_folder="build")
-
-@app.route("/")
-def hello():
-    return render_template('index.html')
